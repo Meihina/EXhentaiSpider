1075a631665e9a8a7c7669adcfa0eb2ec943bdd3
Kaza <50244072+Meihina@users.noreply.github.com>
2020-02-07 13:15:54 +0800

    加头

--- ex_head.py
+++ ex_head.py
@@ -3,6 +3,8 @@ from random import *
 head_arr = [  #请求头
     'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0'
 ]
 
 def random_head() :
